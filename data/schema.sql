05cdd69479833da6707f65d38d811f5366dc7890
Darsha Anuradha <darsha.anuradha2020@gmail.com>
2026-04-22 11:47:34 +0530

    create courses and students tables in schema

--- data/schema.sql
+++ data/schema.sql
@@ -16,3 +16,20 @@ CREATE TABLE lecturers (
     dept_id INT,
     FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE SET NULL
 );
+
+-- 3. Courses Table
+CREATE TABLE courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY, 
+    course_name VARCHAR(150) NOT NULL,
+    lecturer_id INT,
+    total_enrolled INT DEFAULT 0,
+    FOREIGN KEY (lecturer_id) REFERENCES lecturers(lecturer_id)
+);
+
+-- 4. Students Table
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    contact_email VARCHAR(100) UNIQUE NOT NULL
+);
