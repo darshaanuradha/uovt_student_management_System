9b977e8f146045838d2ea8f2bf98a4724a4ce314
Isuru-Lakshitha <isuru456123@gmail.com>
2026-04-25 21:29:54 +0530

    Initial commit - Fixed MySQL port conflict

--- application/db.php
+++ application/db.php
@@ -1,10 +1,13 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$pass = ''; 
+$pass = '';
 $dbname = 'uovt_sms';
+$port = 3307; // Define the new port here
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+// Add $port as the 5th parameter
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
