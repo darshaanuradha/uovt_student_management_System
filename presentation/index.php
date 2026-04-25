315cc95750b9fc58e78fd8845e2c41c061212b76
Darsha Anuradha <darsha.anuradha2020@gmail.com>
2026-04-25 08:43:40 +0530

    refactor:

--- presentation/index.php
+++ presentation/index.php
@@ -3,7 +3,6 @@ session_start();
 
 $page = $_GET['page'] ?? 'login';
 
-// If not logged in and trying to access a secure page, redirect to the router's login
 if (!isset($_SESSION['user_id']) && $page !== 'login') {
     header("Location: index.php?page=login"); 
     exit;
@@ -24,8 +23,7 @@ if (!in_array($page, $allowed_pages)) {
 
 $content = "pages/$page.php";
 
-// If it's the login page, just load the standalone file. 
-// Otherwise, load the secure layout with the sidebar.
+
 if ($page === 'login') {
     require $content;
 } else {
