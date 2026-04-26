c565c57e979d5b5b400fd338aaa9158f29609516
Isuru-Lakshitha <isuru456123@gmail.com>
2026-04-26 21:04:32 +0530

    feat: initialize database connection script with custom port configuration

--- application/db.php
+++ application/db.php
@@ -5,7 +5,6 @@ $pass = '';
 $dbname = 'uovt_sms';
 $port = 3307; // Define the new port here
 
-// Add $port as the 5th parameter
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
